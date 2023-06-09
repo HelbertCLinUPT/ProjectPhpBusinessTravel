0b37c151433f56a9b31856fc31f041b799f83f68
HelbertCLinUPT <hc2020067571@virtual.upt.pe>
2023-06-09 11:46:27 -0500

    nuevo

--- View/header/headerDashboard.php
+++ View/header/headerDashboard.php
@@ -1,7 +1,5 @@
 <?php
-if (@!isset($_SESSION)) {
-    session_start();
-}
+session_start();
 
 if (@$_SESSION['rol'] != 2) {
     header("Location:MainController.php?action=main-index");
@@ -9,6 +7,7 @@ if (@$_SESSION['rol'] != 2) {
 }
 
 ?>
+
 <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
     <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
         <i class="fa fa-bars"></i>
