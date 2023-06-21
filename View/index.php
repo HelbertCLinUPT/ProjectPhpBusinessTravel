326c7faa84f73aa07e6c46e40abfc884be247241
Erick Mauricio <102829461+Erick03072003@users.noreply.github.com>
2023-06-21 03:19:30 -0500

    Update index.php

--- View/index.php
+++ View/index.php
@@ -1,132 +1,140 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>INICIO</title>
-  <link rel="icon" href="View/Pagina/img/Fevicon.png" type="image/png">
-  <link rel="stylesheet" href="View/Pagina/vendors/bootstrap/bootstrap.min.css">
-  <link rel="stylesheet" href="View/Pagina/css/style.css">
-  <script src="View/Pagina/js/facebookapi.js"></script>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>Dashboard</title>
+    <link href="View/static/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+    <link href="View/static/css/sb-admin-2.min.css" rel="stylesheet">
 </head>
 
-<body class="bg-shape">
-  <?php
-  include 'View/header/headerPagina.php';
-  ?>
-  <!--================Hero Banner Area Start =================-->
-  <section class="hero-banner magic-ball">
-    <div class="container">
+<body id="page-top">
+    <div id="wrapper">
+        <?php
+        include 'View/header/lateralDashboard.php';
+        ?>
+        <div id="content-wrapper" class="d-flex flex-column">
+            <div id="content">
+                <?php
+                include 'View/header/headerDashboard.php';
+                ?>
+                <div class="container-fluid">
+                    <div class="d-sm-flex align-items-center justify-content-between mb-4">
+                        <h1 class="h3 mb-0 text-gray-800">Dashboard</h1>
+                    </div>
+                    <div class="row">
+                        <div class="col-xl-3 col-md-6 mb-4">
+                            <div class="card border-left-primary shadow h-100 py-2">
+                                <div class="card-body">
+                                    <div class="row no-gutters align-items-center">
+                                        <div class="col mr-2">
+                                            <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
+                                                Earnings (Monthly)</div>
+                                            <div class="h5 mb-0 font-weight-bold text-gray-800">$40,000</div>
+                                        </div>
+                                        <div class="col-auto">
+                                            <i class="fas fa-calendar fa-2x text-gray-300"></i>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-xl-3 col-md-6 mb-4">
+                            <div class="card border-left-success shadow h-100 py-2">
+                                <div class="card-body">
+                                    <div class="row no-gutters align-items-center">
+                                        <div class="col mr-2">
+                                            <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
+                                                Earnings (Annual)</div>
+                                            <div class="h5 mb-0 font-weight-bold text-gray-800">$215,000</div>
+                                        </div>
+                                        <div class="col-auto">
+                                            <i class="fas fa-dollar-sign fa-2x text-gray-300"></i>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
 
-      <div class="row align-items-center text-center text-md-left">
-        <div class="col-md-6 col-lg-5 mb-5 mb-md-0">
-          <h1>Viajes de calidad con Business Travel</h1>
-          <p>"Descubre un mundo de experiencias con Business Travel. Encuentra los destinos más fascinantes, los hoteles más exclusivos y las mejores ofertas para tus viajes de negocios. ¡Viaja con estilo y comodidad con nosotros!" </p>
-          <a class="button button-hero mt-4" href="#">Ver las Ofertas</a>
+                        <div class="col-xl-3 col-md-6 mb-4">
+                            <div class="card border-left-info shadow h-100 py-2">
+                                <div class="card-body">
+                                    <div class="row no-gutters align-items-center">
+                                        <div class="col mr-2">
+                                            <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Tasks
+                                            </div>
+                                            <div class="row no-gutters align-items-center">
+                                                <div class="col-auto">
+                                                    <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">50%</div>
+                                                </div>
+                                                <div class="col">
+                                                    <div class="progress progress-sm mr-2">
+                                                        <div class="progress-bar bg-info" role="progressbar" style="width: 50%" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="col-auto">
+                                            <i class="fas fa-clipboard-list fa-2x text-gray-300"></i>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-xl-3 col-md-6 mb-4">
+                            <div class="card border-left-warning shadow h-100 py-2">
+                                <div class="card-body">
+                                    <div class="row no-gutters align-items-center">
+                                        <div class="col mr-2">
+                                            <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">
+                                                Pending Requests</div>
+                                            <div class="h5 mb-0 font-weight-bold text-gray-800">18</div>
+                                        </div>
+                                        <div class="col-auto">
+                                            <i class="fas fa-comments fa-2x text-gray-300"></i>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <div class="col-lg-12 mb-4">
+                            <!-- Illustrations -->
+                            <div class="card shadow mb-4">
+                                <div class="card-header py-3">
+                                    <h6 class="m-0 font-weight-bold text-primary">Illustrations</h6>
+                                </div>
+                                <div class="card-body">
+                                    <div class="text-center">
+                                        <img class="img-fluid px-3 px-sm-4 mt-3 mb-4" style="width: 25rem;" src="View/static/img/undraw_posting_photo.svg" alt="...">
+                                    </div>
+                                    <p>Add some quality, svg illustrations to your project courtesy of a
+                                        constantly updated collection of beautiful svg images that you can use
+                                        completely free and without attribution!</p>
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-        <div class="col-md-6 col-lg-7 col-xl-6 offset-xl-1">
-          <img class="img-fluid" src="View/Pagina/img/home/hero-img.png" alt="">
-        </div>
-      </div>
     </div>
-  </section>
-  <!--================Hero Banner Area End =================-->
 
+    <a class="scroll-to-top rounded" href="#page-top">
+        <i class="fas fa-angle-up"></i>
+    </a>
+    <script src="View/static/vendor/jquery/jquery.min.js"></script>
+    <script src="View/static/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="View/static/js/sb-admin-2.min.js"></script>
 
-  <!--================Service Area Start =================-->
-  <section class="section-margin generic-margin">
-    <div class="container">
-      <div class="section-intro text-center pb-90px">
-        <img class="section-intro-img" src="View/Pagina/img/home/section-icon.png" alt="">
-        <h2>Nuestros servicios más populares</h2>
-        <p>"Descubre nuestros servicios más populares. Ofrecemos una amplia selección de experiencias de viaje inolvidables que se adaptan a tus gustos y necesidades. Desde destinos exóticos hasta escapadas urbanas, encuentra los servicios más buscados y crea recuerdos inolvidables con nosotros."</p>
-      </div>
-
-      <div class="row">
-        <div class="col-md-6 col-lg-4 mb-4 mb-lg-0">
-          <div class="service-card text-center">
-            <div class="service-card-img">
-              <img class="img-fluid" src="View/Pagina/img/home/service1.png" alt="">
-            </div>
-            <div class="service-card-body">
-              <br><br><br><br><br><br>
-              <h3>Reserva de Hotel</h3>
-              <p>¡Encuentra los mejores hoteles para tu próxima aventura aquí!
-              </p>
-            </div>
-          </div>
-        </div>
-        <div class="col-md-6 col-lg-4 mb-4 mb-lg-0">
-          <div class="service-card text-center">
-            <div class="service-card-img">
-              <img class="img-fluid" src="View/Pagina/img/home/service2.png" alt="">
-            </div>
-            <div class="service-card-body">
-              <br><br><br><br><br><br>
-              <h3>Reserva de Vuelos</h3>
-              <p>¡Descubre los vuelos perfectos para tu próxima aventura aquí!
-              </p>
-            </div>
-          </div>
-        </div>
-        <div class="col-md-6 col-lg-4 mb-4 mb-lg-0">
-          <div class="service-card text-center">
-            <div class="service-card-img">
-              <img class="img-fluid" src="View/Pagina/img/home/service3.png" alt="">
-            </div>
-            <div class="service-card-body">
-              <br><br><br><br><br><br>
-              <h3>Reserva en Destino</h3>
-              <p>"Reserva tu destino soñado con nosotros".
-              </p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </section>
-  <!--================Service Area End =================-->
-  <!--================About Area Start =================-->
-  <section class="bg-gray section-padding magic-ball magic-ball-about">
-    <div class="container">
-      <div class="row">
-        <div class="col-lg-7 col-md-6">
-          <div class="about-img">
-            <img class="img-fluid" src="View/Pagina/img/home/regalos-para-viajeros.jpg" alt="">
-          </div>
-        </div>
-        <div class="col-lg-5 col-md-6 align-self-center about-content">
-          <h2>Explora la belleza <br class="d-none d-xl-block"> infinita de <br class="d-none d-xl-block"> of the
-            nuestro Perú</h2>
-          <p>Explora los destinos más emblemáticos del Perú. Desde la enigmática ciudad de Machu Picchu hasta la majestuosidad de la ciudad blanca de Arequipa, pasando por la misteriosa belleza de la ciudad de Cusco y la vibrante metrópolis de Lima. Sumérgete en la cultura, historia y naturaleza de destinos como el Cañón del Colca, el Lago Titicaca, la Amazonía peruana y las líneas de Nazca. Con nuestros paquetes turísticos, podrás descubrir la diversidad y riqueza de todo nuestro hermoso Perú."</p>
-
-        </div>
-      </div>
-    </div>
-  </section>
-  <!--================About Area End =================-->
-
-
-
-
-
-  <!--================Blog section Start =================-->
-  <section class="section-padding bg-gray">
-    <div class="container">
-      <div class="section-intro text-center pb-90px">
-        <img class="section-intro-img" src="View/Pagina/img/home/section-icon.png" alt="">
-        <h2>NUESTRAS ULTIMAS PUBLICACIONES EN FACEBOOK</h2>
-      </div>
-      <div id="card-container" class="row"></div>
-
-  </section>
-  <script src="https://www.gstatic.com/dialogflow-console/fast/messenger/bootstrap.js?v=1"></script>
-  <df-messenger chat-icon="https:&#x2F;&#x2F;encrypted-tbn0.gstatic.com&#x2F;images?q=tbn:ANd9GcSfh5kDV55gRWX1BkTRE-4ebyditNq2kd1Izw&ampusqp=CAU" chat-title="businesstravelagency" agent-id="de6a8fd6-d0e8-4ff7-810e-2b5c4112a2f5" language-code="es"></df-messenger>
-  <?php
-  include 'View/header/footerPagina.php';
-  ?>
 </body>
 
 </html>
