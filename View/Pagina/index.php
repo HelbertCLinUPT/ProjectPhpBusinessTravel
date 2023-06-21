8b8f5103187b8773930623235c372c5a975739ff
Erick Mauricio <102829461+Erick03072003@users.noreply.github.com>
2023-06-21 03:21:30 -0500

    Cambios facebookapi page

--- View/Pagina/index.php
+++ View/Pagina/index.php
@@ -5,11 +5,13 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Safario Travel - Home</title>
+  <title>INICIO</title>
   <link rel="icon" href="View/Pagina/img/Fevicon.png" type="image/png">
   <link rel="stylesheet" href="View/Pagina/vendors/bootstrap/bootstrap.min.css">
   <link rel="stylesheet" href="View/Pagina/css/style.css">
+  <script src="View/Pagina/js/facebookapi.js"></script>
 </head>
+
 <body class="bg-shape">
   <?php
   include 'View/header/headerPagina.php';
@@ -62,7 +64,7 @@
               <img class="img-fluid" src="View/Pagina/img/home/service2.png" alt="">
             </div>
             <div class="service-card-body">
-              <br><br><br><br><br><br>  
+              <br><br><br><br><br><br>
               <h3>Reserva de Vuelos</h3>
               <p>¡Descubre los vuelos perfectos para tu próxima aventura aquí!
               </p>
@@ -75,7 +77,7 @@
               <img class="img-fluid" src="View/Pagina/img/home/service3.png" alt="">
             </div>
             <div class="service-card-body">
-             <br><br><br><br><br><br>
+              <br><br><br><br><br><br>
               <h3>Reserva en Destino</h3>
               <p>"Reserva tu destino soñado con nosotros".
               </p>
@@ -90,98 +92,36 @@
   <section class="bg-gray section-padding magic-ball magic-ball-about">
     <div class="container">
       <div class="row">
-        <div class="col-lg-7 col-md-6 mb-4 mb-md-0">
+        <div class="col-lg-7 col-md-6">
           <div class="about-img">
             <img class="img-fluid" src="View/Pagina/img/home/regalos-para-viajeros.jpg" alt="">
           </div>
         </div>
         <div class="col-lg-5 col-md-6 align-self-center about-content">
           <h2>Explora la belleza <br class="d-none d-xl-block"> infinita de <br class="d-none d-xl-block"> of the
-           nuestro Perú</h2>
+            nuestro Perú</h2>
           <p>Explora los destinos más emblemáticos del Perú. Desde la enigmática ciudad de Machu Picchu hasta la majestuosidad de la ciudad blanca de Arequipa, pasando por la misteriosa belleza de la ciudad de Cusco y la vibrante metrópolis de Lima. Sumérgete en la cultura, historia y naturaleza de destinos como el Cañón del Colca, el Lago Titicaca, la Amazonía peruana y las líneas de Nazca. Con nuestros paquetes turísticos, podrás descubrir la diversidad y riqueza de todo nuestro hermoso Perú."</p>
-          
+
         </div>
       </div>
     </div>
   </section>
   <!--================About Area End =================-->
-
-
-
-
-
   <!--================Blog section Start =================-->
   <section class="section-padding bg-gray">
     <div class="container">
       <div class="section-intro text-center pb-90px">
         <img class="section-intro-img" src="View/Pagina/img/home/section-icon.png" alt="">
-        <h2>NUESTOS PAQUETES MAS PEDIDOS</h2>
-        <p>"Descubre nuestros paquetes más solicitados, diseñados especialmente para brindarte experiencias inolvidables. Sumérgete en aventuras fascinantes, deleita tus sentidos con destinos asombrosos y crea recuerdos inigualables. ¡Prepárate para vivir momentos inolvidables con nuestros paquetes turísticos más populares!"</p>
+        <h2>NUESTRAS ULTIMAS PUBLICACIONES EN FACEBOOK</h2>
       </div>
+      <div id="card-container" class="row"></div>
 
-      <div class="row">
-        <div class="col-md-6 col-lg-4 mb-4 mb-lg-0">
-          <div class="card-blog">
-          <img class="card-img rounded-0" src="View/Pagina/img/blog/puntacana.jpg" alt="" style="width: 100%;">
-            <div class="card-blog-body">
-              <a href="#">
-                <h4>¡Explora el paraíso en nuestro paquete turístico a Punta Cana!</h4>
-              </a>
-              <ul class="card-blog-info">
-                <li><a href="#"><span class="align-middle"><i class="ti-notepad"></i></span>Jan 03, 2018</a></li>
-                <li><a href="#"><span class="align-middle"><i class="ti-comments-smiley"></i></span>03 Comments</a></li>
-              </ul>
-              <p>"Sumérgete en el paraíso de Punta Cana con nuestro exclusivo paquete turístico. Disfruta de playas de arena blanca, aguas cristalinas y experiencias inolvidables. ¡Vive la aventura de tu vida en uno de los destinos más espectaculares del Caribe con nuestro paquete turístico a Punta Cana!" </p>
-            </div>
-          </div>
-        </div>
-       
-        <!-- <iframe src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2FBusinessTravelTacna%2Fposts%2Fpfbid02mg4YDH6emv6xBt6HaeXRHbsxrHP58DLzcUNPcGn6nWkhcvkARdHNKfzwNxe84Qgul&show_text=true&width=500" width="500" height="628" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe> -->
-
-        <div class="col-md-6 col-lg-4 mb-4 mb-lg-0">
-          <div class="card-blog">
-            <img class="card-img rounded-0" src="View/Pagina/img/blog/disneyland_01.jpg" alt="">
-            <div class="card-blog-body">
-              <a href="#">
-                <h4> ¡Vive la magia en nuestro paquete turístico a Disney!</h4>
-              </a>
-              <ul class="card-blog-info">
-                <li><a href="#"><span class="align-middle"><i class="ti-notepad"></i></span>Jan 03, 2018</a></li>
-                <li><a href="#"><span class="align-middle"><i class="ti-comments-smiley"></i></span>03 Comments</a></li>
-              </ul>
-              <p>"Sumérgete en la magia de Disney con nuestro paquete turístico único. Disfruta de emocionantes atracciones, personajes icónicos y momentos inolvidables en el lugar más mágico del mundo. ¡Embárcate en un viaje lleno de diversión y fantasía con nuestro paquete turístico a Disney!"</p>
-            </div>
-          </div>
-        </div>
-
-        <div class="col-md-6 col-lg-4 mb-4 mb-lg-0">
-          <div class="card-blog">
-            <img class="card-img rounded-0" src="View/Pagina/img/blog/9.jpg" alt="">
-            <div class="card-blog-body">
-              <a href="#">
-                <h4> ¡Sumérgete en la grandeza de la Torre Eiffel con nuestro paquete turístico!</h4>
-              </a>
-              <ul class="card-blog-info">
-                <li><a href="#"><span class="align-middle"><i class="ti-notepad"></i></span>Jan 03, 2018</a></li>
-                <li><a href="#"><span class="align-middle"><i class="ti-comments-smiley"></i></span>03 Comments</a></li>
-              </ul>
-              <p>
-                "Descubre la majestuosidad de la Torre Eiffel. Maravíllate con su imponente estructura y disfruta de las vistas panorámicas de París desde lo más alto. Vive una experiencia inigualable en uno de los íconos más reconocidos del mundo. ¡Ven y déjate cautivar por la magia de la Torre Eiffel!"  </p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
   </section>
   <script src="https://www.gstatic.com/dialogflow-console/fast/messenger/bootstrap.js?v=1"></script>
-  <df-messenger
-    chat-icon="https:&#x2F;&#x2F;encrypted-tbn0.gstatic.com&#x2F;images?q=tbn:ANd9GcSfh5kDV55gRWX1BkTRE-4ebyditNq2kd1Izw&ampusqp=CAU"
-    chat-title="businesstravelagency"
-    agent-id="de6a8fd6-d0e8-4ff7-810e-2b5c4112a2f5"
-    language-code="es"
-  ></df-messenger>
- <?php
-   include 'View/header/footerPagina.php';
- ?>
+  <df-messenger chat-icon="https:&#x2F;&#x2F;encrypted-tbn0.gstatic.com&#x2F;images?q=tbn:ANd9GcSfh5kDV55gRWX1BkTRE-4ebyditNq2kd1Izw&ampusqp=CAU" chat-title="businesstravelagency" agent-id="de6a8fd6-d0e8-4ff7-810e-2b5c4112a2f5" language-code="es"></df-messenger>
+  <?php
+  include 'View/header/footerPagina.php';
+  ?>
 </body>
+
 </html>
